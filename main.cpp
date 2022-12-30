10b46883b18d5fa31d00820cfc8f37e6055dbced
Антон Брыляков <yoll@rambler.ru>
2022-12-30 04:54:42 -0800

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include "print_container.hpp"
+#ifdef _WIN32
+#include "windows.h"
+#endif
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+#ifdef _WIN32
+	SetConsoleCP(1251);
+#endif
+
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set); // four one three two. помните почему такой порядок? :)
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list); // one, two, three, four
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector); // one, two, three, four
+
+}
---
+++ print_container.hpp
@@ -0,0 +1,12 @@
+﻿template <typename T>
+void print_container(const T& container)
+{
+	auto it = container.cbegin();
+	while (it != container.cend())
+	{
+		std::cout << *it << " ";
+		++it;
+	}
+
+	std::cout << std::endl;
+}
